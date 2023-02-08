74c72a894357bceae039c273e3d61cb28c396acb
Yusuf1208 <yusufsahin1208@gmail.com>
2023-02-09 02:44:01 +0300

    TrenKayit
    It is an HTTP API that can pull data from Microsoft SQL database and respond to HTTP Post and HTTP Get requests. This application records the person's name, city, boarding date and ticket price on the train to the SQL platform and brings the person information to the API by entering the personnel ID information.

---
+++ TrenKayitAPI/Controllers/PersonController.cs
@@ -0,0 +1,67 @@
+﻿using DatabaseProject.Interfaces;
+using DatabaseProject.Models;
+using DatabaseProject.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrenKayitAPI.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _PersonRepository;
+
+        public PersonController(IPersonRepository personRepository)
+        {
+            _PersonRepository = personRepository;
+        }
+
+        [HttpGet]
+
+        public ActionResult GetPersons()
+        {
+            try
+            {
+                var persons = _PersonRepository.GetPerson();
+                return Ok(persons);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+        
+        [HttpGet]
+
+        public ActionResult GetPersonById(int Id)
+        {
+            try
+            {
+                var person = _PersonRepository.GetPersonById(Id);
+                return Ok(person);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public ActionResult AddPerson(Person person)
+        {
+            try
+            {
+                var addedPerson = _PersonRepository.AddPerson(person);
+                return Ok(addedPerson);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+
+        }
+    }
+
+}
